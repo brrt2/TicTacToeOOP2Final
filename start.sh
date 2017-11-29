88f37ede7e05c8d60a2e1495ec2c963d60bd7f5c
Bartosz Pieczara <bartosz_pieczara@epam.com>
2017-11-29 14:58:52 +0100

    made amendments to the start script

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 
 cd /home/bartoszpieczara/IdeaProjects/GameForDemo
-git clone https://github.com/brrt2/TicTacToeOOP2.git
-cd /home/bartoszpieczara/IdeaProjects/GameForDemo/TicTacToeOOP2
+git clone https://github.com/brrt2/TicTacToeOOP2Final.git
+cd /home/bartoszpieczara/IdeaProjects/GameForDemo/TicTacToeOOP2Final
 mvn install
 java -jar target/TicTacToeOOP-1.0-SNAPSHOT.jar
