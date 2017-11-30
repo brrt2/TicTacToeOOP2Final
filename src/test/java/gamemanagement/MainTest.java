5c7ee0f225567b442a3a8014a60e6ac462743503
Bartosz Pieczara <bartosz_pieczara@epam.com>
2017-11-30 08:57:40 +0100

    further fixed end to end tests

--- src/test/java/gamemanagement/MainTest.java
+++ src/test/java/gamemanagement/MainTest.java
@@ -36,9 +36,9 @@ public class MainTest {
         return new Object[][]{
 
                 {"o\ne\nbartek\nadam\n3\n3\n3\nx\n2\n3\na\n1\n4\n2\n5\n3\nn","Do you wish to continue Y/N ?"},
-//                {"o\ne\nroman\nandrzej\n4\n4\n3\nx\n1\n3\na\n1\n4\n2\n5\n3\nn\n", "Do you want to play another match Y/N ?"},
-//                {"o\np\nWojtek\nMichal\n4\n4\n3\nx\n1\n3\na\n1\n4\n2\n5\n3\nn\n", "Czy chcesz zagrac kolejny mecz y-tak /n-nie ?"},
-//                {"o\ns\nWojtek\nMichal\n4\n4\n3\nx\n1\n3\na\n1\n4\n2\n5\n3\nn\n", "Desea jugar otra vez y/n ?"},
+              {"o\ne\nroman\nandrzej\n4\n4\n3\nx\n2\n3\na\n1\n4\n2\n5\n3\nn", "Do you wish to continue Y/N ?"},
+                {"o\np\nWojtek\nMichal\n4\n4\n3\nx\n2\n3\na\n1\n4\n2\n5\n3\nn", "Czy chcesz kontynuowac  y-tak/n-nie ?"},
+               {"o\ns\nWojtek\nMichal\n4\n4\n3\nx\n2\n3\na\n1\n4\n2\n5\n3\nn", "Desea continuar  y-si /n -no ?"},
         };
     }
 
@@ -52,13 +52,12 @@ public class MainTest {
         System.setIn(new ByteArrayInputStream(sequence.getBytes()));
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(byteArrayOutputStream);
-       System.setOut(ps);
+        System.setOut(ps);
         Main.main(new String[]{});
        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
         List<String> gameOutput = Arrays.asList(byteArrayOutputStream.toString().split("\n"));
         String actualLastLine = gameOutput.get(gameOutput.size() - 1);
         fw.write(Collections.singletonList(byteArrayOutputStream.toString()).toString());
-
         assertEquals(actualLastLine, expectedResult);
     }
 }
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 
 cd /home/bartoszpieczara/IdeaProjects/GameForDemo
-git clone https://github.com/brrt2/ForDemo.git
-cd /home/bartoszpieczara/IdeaProjects/GameForDemo/ForDemo
+git clone https://github.com/brrt2/TicTacToeOOP2Final.git
+cd /home/bartoszpieczara/IdeaProjects/GameForDemo/TicTacToeOOP2Final
 mvn install
 java -jar target/TicTacToeOOP-1.0-SNAPSHOT.jar
